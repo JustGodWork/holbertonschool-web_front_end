1d301949760dcc8f5d4e6fbe2b4cde0b680dc542
JustGod <85418813+JustGodWork@users.noreply.github.com>
2024-12-03 17:04:48 +0100

    Add function to count prime numbers and measure execution time

---
+++ Javascript_advanced/10-prime.js
@@ -0,0 +1,25 @@
+function countPrimeNumbers() {
+  let count = 0;
+  for (let i = 2; i <= 100; i++) {
+    let isPrime = true;
+    for (let j = 2; j < i; j++) {
+      if (i % j === 0) {
+        isPrime = false;
+        break;
+      }
+    }
+    if (isPrime) {
+      count++;
+    }
+  }
+  return count;
+}
+
+const start = performance.now();
+
+for (let i = 0; i < 100; i++) {
+  countPrimeNumbers();
+}
+
+const end = performance.now();
+console.log(`Execution time of calculating prime numbers 100 times was ${end - start} milliseconds`);
