f6a75b6cc00bed6ad5be000f1015b97b308e203e
JustGod <85418813+JustGodWork@users.noreply.github.com>
2024-12-03 17:04:22 +0100

    Add studentHogwarts class to manage student scores with private properties

---
+++ Javascript_advanced/6-hogwarts.js
@@ -0,0 +1,40 @@
+class studentHogwarts {
+  #privateScore = 0;
+  #name = null;
+
+  #changeScoreBy(points) {
+    this.#privateScore += points;
+  }
+
+  setName(newName) {
+    this.#name = newName;
+  }
+
+  rewardStudent() {
+    this.#changeScoreBy(1);
+  }
+
+  penalizeStudent() {
+    this.#changeScoreBy(-1);
+  }
+
+  getScore() {
+    return `${this.#name}: ${this.#privateScore}`;
+  }
+}
+
+const harry = new studentHogwarts();
+harry.setName('Harry');
+harry.rewardStudent();
+harry.rewardStudent();
+harry.rewardStudent();
+harry.rewardStudent();
+console.log(harry.getScore());
+
+const draco = new studentHogwarts();
+draco.setName('Draco');
+draco.rewardStudent();
+draco.penalizeStudent();
+draco.penalizeStudent();
+draco.penalizeStudent();
+console.log(draco.getScore());
