db7b1516f7dafa1e64a924ea9b9b01d764464585
JustGod <85418813+JustGodWork@users.noreply.github.com>
2025-01-22 10:39:28 +0100

    Add HTML and JavaScript files for cookie management functionality

--- Cookies_local_storage/src/index.js
+++ Cookies_local_storage/src/index.js
@@ -1,8 +1,12 @@
 function setCookies() {
     const firstname = document.getElementById('firstname').value;
     const email = document.getElementById('email').value;
-    document.cookie = `firstname=${firstname}; path=/`;
-    document.cookie = `email=${email}; path=/`;
+    const date = new Date();
+    date.setTime(date.getTime() + (10 * 24 * 60 * 60 * 1000));
+    const expires = `expires=${date.toUTCString()}`;
+
+    document.cookie = `firstname=${firstname}; ${expires}; path=/`;
+    document.cookie = `email=${email}; ${expires}; path=/`;
 }
 
 function showCookies() {
@@ -13,6 +17,6 @@ function showCookies() {
     }, {});
 
     const p = document.createElement('p');
-    p.innerHTML = `Cookies: ${JSON.stringify(cookies)}`;
+    p.innerHTML = `Email: ${email} - Firstname: ${firstname}`;
     document.body.appendChild(p);
 }
